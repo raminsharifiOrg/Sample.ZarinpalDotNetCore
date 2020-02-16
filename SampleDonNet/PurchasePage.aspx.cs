e9da99bcc45090df83c11c417be2cde6248fc11e
Ramin Sharifi <rm.sharifi1381@gmail.com>
2020-02-16 16:25:09 +0330

    Add project files.

---
+++ SampleDonNet/PurchasePage.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ZarinPal; //import ZarinPal Assembly for us ZarinPal Classes
+
+public partial class PurchasePage : System.Web.UI.Page
+{
+    
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+         ZarinPal.ZarinPal zarinpal = ZarinPal.ZarinPal.Get();
+
+         String MerchantID = "71c705f8-bd37-11e6-aa0c-000c295eb8fc";
+         String CallbackURL = "http://localhost:59701/VerficationPage.aspx";
+         long Amount = 100;
+         String Description = "This is Test Payment";
+
+         ZarinPal.PaymentRequest pr = new ZarinPal.PaymentRequest(MerchantID, Amount, CallbackURL, Description);
+
+         zarinpal.EnableSandboxMode();
+         var res = zarinpal.InvokePaymentRequest(pr);
+            if (res.Status == 100) {
+                Response.Redirect(res.PaymentURL);
+            }
+          
+    }
+}
---
+++ SampleDonNet/VerficationPage.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class VerficationPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+        var collection = HttpUtility.ParseQueryString(this.ClientQueryString);
+            String Status = collection["Status"];
+
+
+            if (Status != "OK")
+            {
+                Response.Write("<script>alert('Purchase unsuccessfully')</script>");
+                return;
+            }
+
+
+
+            var zarinpal = ZarinPal.ZarinPal.Get();
+            zarinpal.EnableSandboxMode();
+            String Authority = collection["Authority"];
+            String MerchantID = "71c705f8-bd37-11e6-aa0c-000c295eb8fc";
+            long Amount = 100;
+
+
+            var verificationRequest = new ZarinPal.PaymentVerification(MerchantID , Amount , Authority);
+            var verificationResponse = zarinpal.InvokePaymentVerification(verificationRequest);
+            if (verificationResponse.Status == 100)
+            {
+                Response.Write(String.Format("<script>alert('Purchase successfully with ref transaction {0}')</script>", verificationResponse.RefID));
+            }
+            else {
+
+                Response.Write(String.Format("<script>alert('Purchase unsuccessfully Error code is: {0}')</script>",verificationResponse.Status));
+
+            }
+
+
+    }
+}
